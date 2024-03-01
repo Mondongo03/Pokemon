066b576bdc7dce600db42bd5b3625f732774de5a
ruben <rjulian@elpuig.xeill.net>
2024-03-01 03:25:53 +0100

    Lucario Terminado

--- app/src/main/java/com/example/pokemon_fv/LucarioFragment.java
+++ app/src/main/java/com/example/pokemon_fv/LucarioFragment.java
@@ -72,8 +72,8 @@ public class LucarioFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
         BottomNavigationView navView = requireActivity().findViewById(R.id.nav_view);
-        navView.setVisibility(View.INVISIBLE);
 
+            navView.setVisibility(View.INVISIBLE);
     }
 
     @Override
@@ -82,48 +82,9 @@ public class LucarioFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentLucarioBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        /*lucarioStatsChart = root.findViewById(R.id.lucarioStatsChart);
-
-        // Crear datos de ejemplo
-        List<BarEntry> entries1 = new ArrayList<>();
-        entries1.add(new BarEntry(0, new float[]{80f, 60f}));
-        entries1.add(new BarEntry(1, new float[]{70f, 80f}));
-        entries1.add(new BarEntry(2, new float[]{90f, 70f}));
-        entries1.add(new BarEntry(3, new float[]{75f, 85f}));
-        entries1.add(new BarEntry(4, new float[]{65f, 75f}));
-        entries1.add(new BarEntry(5, new float[]{85f, 95f}));
-
-        BarDataSet dataSet1 = new BarDataSet(entries1, "Lucario 1");
-        dataSet1.setColors(new int[]{Color.RED, Color.BLUE}); // Colores para cada conjunto de datos
-
-        // Configurar el gráfico
-        BarData barData = new BarData(dataSet1);
-        lucarioStatsChart.setData(barData);
-        lucarioStatsChart.setDrawValueAboveBar(false);
-
-        XAxis xAxis = lucarioStatsChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-
-        YAxis leftAxis = lucarioStatsChart.getAxisLeft();
-        leftAxis.setAxisMinimum(0f);
-
-        YAxis rightAxis = lucarioStatsChart.getAxisRight();
-        rightAxis.setAxisMinimum(0f);
-
-        Legend legend = lucarioStatsChart.getLegend();
-        legend.setForm(Legend.LegendForm.SQUARE);
-        legend.setFormSize(10f);
-        legend.setTextSize(12f);
-        legend.setXEntrySpace(5f);
-
-        lucarioStatsChart.invalidate();*/
-
         ImageView back = root.findViewById(R.id.back);
 
         ImageView imageView = root.findViewById(R.id.gifLucario);
-
-
 
 // Cargar y mostrar un GIF desde una URL
         Glide.with(this).asGif().load(R.drawable.lucario_gif).into(imageView);
@@ -138,35 +99,4 @@ public class LucarioFragment extends Fragment {
 
         return root;
     }
-
-
-    /*private void setupPieChart() {
-        // Crear un gráfico circular (pie chart)
-        lucarioStatsPlot.clear();
-
-        // Configurar los datos de ejemplo
-        SegmentFormatter segmentFormatter = new SegmentFormatter(Color.RED);
-        Segment segment1 = new Segment("Ataque", 80);
-        Segment segment2 = new Segment("Defensa", 70);
-        Segment segment3 = new Segment("Vida", 90);
-        Segment segment4 = new Segment("Ataque Especial", 75);
-        Segment segment5 = new Segment("Defensa Especial", 65);
-        Segment segment6 = new Segment("Velocidad", 85);
-
-        // Añadir los segmentos al gráfico circular
-        lucarioStatsPlot.addSegment(segment1, segmentFormatter);
-        lucarioStatsPlot.addSegment(segment2, segmentFormatter);
-        lucarioStatsPlot.addSegment(segment3, segmentFormatter);
-        lucarioStatsPlot.addSegment(segment4, segmentFormatter);
-        lucarioStatsPlot.addSegment(segment5, segmentFormatter);
-        lucarioStatsPlot.addSegment(segment6, segmentFormatter);
-
-        // Configurar el gráfico circular
-        lucarioStatsPlot.getBorderPaint().setColor(Color.TRANSPARENT);
-        lucarioStatsPlot.getBackgroundPaint().setColor(Color.TRANSPARENT);
-        lucarioStatsPlot.getRenderer(PieRenderer.class).setDonutSize(0, PieRenderer.DonutMode.PERCENT);
-
-        // Mostrar el gráfico
-        lucarioStatsPlot.redraw();
-    }*/
 }
